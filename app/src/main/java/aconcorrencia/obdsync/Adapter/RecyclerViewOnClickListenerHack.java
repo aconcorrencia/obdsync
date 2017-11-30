da33a25ab2406c21a4a76a3d65f709225f6e1fcc
Yuri Marinho Alcântara <yuri@aconcorrencia.com>
2017-11-30 14:49:59 -0300

    as library

--- app/src/main/java/aconcorrencia/obdsync/Adapter/RecyclerViewOnClickListenerHack.java
+++
@@ -1,9 +0,0 @@
-package aconcorrencia.obdsync.Adapter;
-
-import android.view.View;
-
-
-public interface RecyclerViewOnClickListenerHack {
-    public void onClickListener(View view, int position);
-    public void onLongPressClickListener(View view, int position);
-}
